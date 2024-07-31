e99872089d0ddeb0672280bf5eacf72886e8b970
Caleb Grant <geocoug@gmail.com>
2024-07-31 16:32:53 -0700

    cleanup
    Signed-off-by: Caleb Grant <geocoug@gmail.com>

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 osascript -e 'tell application "Microsoft Excel" to close (every workbook whose name is "test.xlsx") saving no' >> /dev/null \
-&& gsi-xtab -d -k -s -f tests/data/sample2.csv -o test.xlsx -r location_id sample_no sample_material labsample meas_basis -c cas_rn analyte analyte_name -v concentration units qualifiers \
+&& crosstab -d -k -s -f tests/data/sample2.csv -o test.xlsx -r location_id sample_no sample_material labsample meas_basis -c cas_rn analyte analyte_name -v concentration units qualifiers \
 && open test.xlsx
