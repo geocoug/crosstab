e0c23abe900a2abe9d3f9407082d6869e19a680d
Caleb Grant <geocoug@gmail.com>
2025-06-18 12:03:58 -0700

    Bump version: 0.0.14 → 0.0.15

--- crosstab/crosstab.py
+++ crosstab/crosstab.py
@@ -23,7 +23,7 @@ __author__ = "Caleb Grant"
 __url__ = "https://github.com/geocoug/crosstab"
 __author_email__ = "grantcaleb22@gmail.com"
 __license__ = "GNU GPLv3"
-__version__ = "0.0.14"
+__version__ = "0.0.15"
 __description__ = "Rearrange data from a normalized CSV format to a crosstabulated format, with styling."
 
 logging.basicConfig(
